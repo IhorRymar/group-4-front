7fdb6f0435ff9be8f50a3be7c560d9b88c12a85e
Siarhei Luchynovich <94983189+SiarheyLu4@users.noreply.github.com>
2022-12-03 16:02:05 +0200

    feature/app add stuled-components global and vars

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,7 +7,6 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
       }}
     >
       React homework template
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import './stylesheet/index.css';
+
+import GlobalStyles from './stylesheet/global';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>
 );
