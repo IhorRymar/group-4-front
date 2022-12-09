b135588536ecf6af7d3fd13de7a0cd6ecd0f799c
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-12-09 19:08:00 +0200

    header spinner bugfix authslice

--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 
 import * as api from '../../services/auth';
 
