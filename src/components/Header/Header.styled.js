6acbd4bd1b0226a092bbc51cedf7327fdcdbe87a
Vasiliy Movchan <movchan.vasia@gmail.com>
2022-12-09 19:36:27 +0200

    bugfix styled header

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -4,7 +4,7 @@ import baseVars from '../../stylesheet/vars';
 export const StyledHeader = styled.div`
   display: flex;
   min-height: 60px;
-  width: calc(100% - 40px);
+  width: 100%;
   justify-content: space-between;
   align-items: center;
   padding: 15px 20px;
@@ -12,12 +12,12 @@ export const StyledHeader = styled.div`
 
   @media screen and (min-width: ${baseVars.sizeScreen.tablet}) {
     min-height: 80px;
-    width: calc(100% - 64px);
+    /* width: calc(100% - 64px); */
     padding: 20px 32px;
   }
 
   @media screen and (min-width: ${baseVars.sizeScreen.desktop}) {
-    width: calc(100% - 32px);
+    /* width: calc(100% - 32px); */
     padding: 20px 16px;
   }
 `;
