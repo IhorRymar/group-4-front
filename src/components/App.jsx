13cfbf98d563af450d770a6706f94da95bbd6890
Nastya Lutsko <nastyalutsko777@gmail.com>
2022-12-10 21:36:07 +0100

    ellipsis

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -34,7 +34,7 @@ export const App = () => {
         <Routes>
           <Route element={<PrivateRoute />}>
             <Route path="/" element={<Home />} />
-            <Route
+            {/* <Route
               index
               element={
                 <div>
@@ -63,7 +63,7 @@ export const App = () => {
                 </main>
               }
             />
-            {/* <Route path="/statistics" element={<Statistics />} /> */}
+            <Route path="/statistics" element={<Statistics />} /> */}
           </Route>
 
           <Route element={<PublicRoute />}>
