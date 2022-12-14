fac160917231bfac830804c90cc85bfd15ae4c49
Ivan Zabolotniy <mustadonte@gmail.com>
2022-12-14 21:01:46 +0200

    fix hometab toggle

--- src/components/HomeTab/HomeTab.jsx
+++ src/components/HomeTab/HomeTab.jsx
@@ -1,61 +1,67 @@
-import { React, Fragment,useEffect, useMemo, useState } from "react";
-import MobileCard from "./MobileCard/MobileCard";
-import TabletTab from "./TabletTab/TabletTab";
+import { React, Fragment, useEffect, useMemo, useState } from 'react';
+import MobileCard from './MobileCard/MobileCard';
+import TabletTab from './TabletTab/TabletTab';
 import Media from 'react-media';
 import { ButtonCircle } from 'components/ButtonCircle/ButtonCircle';
 import { ModalAddTransaction } from 'components/ModalAddTransaction/ModalAddTransaction';
 
-import { useDispatch, useSelector } from "react-redux";
-import { fetchTransactions} from 'redux/transactions/transactions-operation';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchTransactions } from 'redux/transactions/transactions-operation';
 import { getTransactions } from 'redux/transactions/transactions-selector';
-import { toggleModal } from 'redux/modal/modal-sclice';
-import { COLUMNS } from './columns'
+import { isAddModalOpen } from 'redux/modal/modal-sclice';
+import { addModalOpen } from 'redux/modal/modal-selector';
+import { COLUMNS } from './columns';
 import { Pagination } from '@mui/material';
 import styled from 'styled-components';
 import baseVars from 'stylesheet/vars';
 
-
-const HomeTab = () => { 
+const HomeTab = () => {
   const [page, setPage] = useState(1);
   const [transactionsPerPage] = useState(10);
   const columns = useMemo(() => COLUMNS, []);
-  const { result: data, transactionsTotalQuantity } = useSelector(getTransactions);
-  
+  const { result: data, transactionsTotalQuantity } =
+    useSelector(getTransactions);
+
   const dispatch = useDispatch();
 
-  const isModalOpen = useSelector(
-    state => state.global.isAddTransactionModalOpen
-  );
+  const isModalOpen = useSelector(addModalOpen);
 
   const onModal = () => {
-    dispatch(toggleModal(!isModalOpen));
+    dispatch(isAddModalOpen(!isModalOpen));
   };
 
   useEffect(() => {
     // console.log(page + "!!!")
     dispatch(fetchTransactions(page));
   }, [dispatch, page]);
-  
 
   const handleChange = (e, page) => {
-  setPage(page)
-}
-  const totalPages = Math.ceil(transactionsTotalQuantity / transactionsPerPage)
-  const totalPagesInteger =  totalPages ? totalPages : 1
-  
+    setPage(page);
+  };
+  const totalPages = Math.ceil(transactionsTotalQuantity / transactionsPerPage);
+  const totalPagesInteger = totalPages ? totalPages : 1;
+
   return (
     <Con>
-      <Media queries={{
-        small: "(max-width: 767px)",
-        medium: "(min-width: 768px) and (max-width: 1279px)",
-        large: "(min-width: 1280px)"
-      }}>
+      <Media
+        queries={{
+          small: '(max-width: 767px)',
+          medium: '(min-width: 768px) and (max-width: 1279px)',
+          large: '(min-width: 1280px)',
+        }}
+      >
         {matches => (
           <Fragment>
             {matches.small && <MobileCard items={data} columns={columns} />}
-            {matches.medium && <TabletTab items={data} columns={columns}   />}
+            {matches.medium && <TabletTab items={data} columns={columns} />}
             {matches.large && <TabletTab items={data} columns={columns} />}
-            {(transactionsTotalQuantity > 10) ? <Paginate  count={totalPagesInteger} page={page} onChange={handleChange} /> : null }
+            {transactionsTotalQuantity > 10 ? (
+              <Paginate
+                count={totalPagesInteger}
+                page={page}
+                onChange={handleChange}
+              />
+            ) : null}
           </Fragment>
         )}
       </Media>
@@ -65,14 +71,14 @@ const HomeTab = () => {
         <ModalAddTransaction toggleModal={onModal} isOpen={isModalOpen} />
       )}
     </Con>
-    )
-}
+  );
+};
 
-export default HomeTab
+export default HomeTab;
 
 const Con = styled.div`
-position: relative;
-  display:flex;
+  position: relative;
+  display: flex;
   flex-direction: column;
   padding-bottom: 70px;
 
@@ -85,17 +91,20 @@ position: relative;
     padding-top: 32px;
     align-items: flex-end;
   }
-`
+`;
 const Paginate = styled(Pagination)`
   position: absolute;
   bottom: 20px;
-  display:flex;
+  display: flex;
   justify-content: center;
   margin-top: 20px;
   @media screen and (min-width: ${baseVars.sizeScreen.mobile}) {
-    width:280px;}
+    width: 280px;
+  }
   @media screen and (min-width: ${baseVars.sizeScreen.tablet}) {
-    width: 704px;}
-    @media screen and (min-width: ${baseVars.sizeScreen.desktop}) {
-    width: 715px;}
-`
+    width: 704px;
+  }
+  @media screen and (min-width: ${baseVars.sizeScreen.desktop}) {
+    width: 715px;
+  }
+`;
